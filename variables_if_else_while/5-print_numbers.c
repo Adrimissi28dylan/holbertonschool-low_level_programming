966b0dd1193dfcdd6ba7574ea45677534e079c16
Adrimissi28dylan <massiel_ambc@gotmail.com>
2024-10-24 19:44:05 +0000

    Answer tasks 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
