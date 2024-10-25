1755ef35837fd527090b4f9fa14a50f2ebfde16f
Adrimissi28dylan <massiel_ambc@hotmail.com>
2024-10-25 23:07:04 +0000

    Answer tasks 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int a;
+
+	for (c = 0 ; c <= 10 ; c++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+		_putchar('\n');
+	}
+}
