a67c60e1aa26e9ed279743687ba994333d2f9f67
Adrimissi28dylan <massiel_ambc@hotmail.com>
2024-10-25 19:08:05 +0000

    Answer tasks 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-	int n = '0';
+	int d;
+	char low;
 
-	for (n = '0' ; n <= '9' ; n++)
-	{
-		putchar(n);
-	}
-
+	for (d = '0' ; d <= '9' ; d++)
+		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
 	putchar('\n');
 
 	return (0);
